e33656fdcd39640a80a43da533be6535c851648f
seonggon do <mooh2jj@naver.com>
2023-09-17 23:12:37 +0900

    update: Set => person 객체의 equals(), hashCode() 메소드를 재정의

--- src/main/java/com/example/javachallengeprog/controller/PhoneBookController.java
+++ src/main/java/com/example/javachallengeprog/controller/PhoneBookController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -21,22 +22,22 @@ public class PhoneBookController {
     // 3. 전화번호는 010-1234-5678과 같은 형식이다.
     // 4. 전화번호부에 있는 사람의 이름으로 검색할 수 있다.
 //    List<Person> personList = new ArrayList<>();
-    Set<Person> personList = new HashSet<>();
+    Set<Person> phoneBook = new HashSet<>();
 
     public PhoneBookController() {
         // 기본적으로 몇 명의 초기 사람을 추가하고 시작합니다.
         Person person1 = new Person("홍길동");
         person1.addPhoneNumber("010-1234-5678");
         person1.addPhoneNumber("010-1234-5679");
-        personList.add(person1);
+        phoneBook.add(person1);
 
         Person person2 = new Person("김철수");
         person2.addPhoneNumber("010-5555-5555");
-        personList.add(person2);
+        phoneBook.add(person2);
 
         Person person3 = new Person("김영희");
         person3.addPhoneNumber("010-9999-9999");
-        personList.add(person3);
+        phoneBook.add(person3);
     }
 
     @PostMapping("/add")
@@ -45,24 +46,33 @@ public class PhoneBookController {
     ) {
         log.info("personReqDto: {}", personReqDto);
         Person person = new Person(personReqDto.getName());
+        // person 객체의 equals(), hashCode() 메소드를 재정의해야 한다.
+        // name, number가 같으면 같은 객체로 인식하도록
+        // => VO: 순수한 필드 값으로 이루어짐, 그래서 비교도 그 필드 값으로 비교해주게 만들어야 한다.
+
         person.addPhoneNumber(personReqDto.getPhoneNumber());
-        personList.add(person);
-        return personList;
+        phoneBook.add(person);
+        return phoneBook;
     }
 
     @GetMapping("/search")
     public List<Person> search(
-            @RequestParam String name
+            @RequestParam(required = false) String name
     ) {
         log.info("name: {}", name);
         // name에 숫자 또는 특수문자 포함 안돼, 영어, 한글만 되야
         String regex = "[a-zA-Z가-힣]*$";
-        if(!name.matches(regex)) {
-            throw new IllegalArgumentException("이름 형식이 맞지 않습니다.");
-        }
+
         List<Person> result = new ArrayList<>();
-        for (Person person : personList) {
-            if (person.getName().toLowerCase().equals(name.toLowerCase().trim())) {
+        for (Person person : phoneBook) {
+            if (name != null) {
+                if(!name.matches(regex)) {
+                    throw new IllegalArgumentException("이름 형식이 맞지 않습니다.");
+                }
+                if (person.getName().toLowerCase().equals(name.toLowerCase().trim())) {
+                    result.add(person);
+                }
+            } else {
                 result.add(person);
             }
         }
--- src/main/java/com/example/javachallengeprog/entity/Person.java
+++ src/main/java/com/example/javachallengeprog/entity/Person.java
@@ -1,15 +1,18 @@
 package com.example.javachallengeprog.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
+//@EqualsAndHashCode
 public class Person {
 
     private String name;
@@ -27,5 +30,18 @@ public class Person {
             throw new IllegalArgumentException("전화번호 형식이 올바르지 않습니다.");
         }
         this.phoneNumberList.add(phoneNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(phoneNumberList, person.phoneNumberList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumberList);
     }
 }
